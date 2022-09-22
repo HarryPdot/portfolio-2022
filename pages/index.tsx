55dd4aa23d2d1f5fffa34b7a70c76ee2caf04d0a
Harry <harrypham_@hotmail.com>
2022-09-22 18:29:09 +1000

    move files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Home from './src/Components/Home/Home'
-import About from './src/Components/About/About'
-import Projects from './src/Components/Projects/Projects'
-import Contact from './src/Components/Contact/Contact'
-import { appContainer, test, container, gap } from './src/styles/app.css'
+import Home from '../src/Components/Home/Home'
+import About from '../src/Components/About/About'
+import Projects from '../src/Components/Projects/Projects'
+import Contact from '../src/Components/Contact/Contact'
+import { appContainer, test, container, gap } from '../src/styles/app.css'
 import clsx from 'clsx';
 
 const App: any = () => {
