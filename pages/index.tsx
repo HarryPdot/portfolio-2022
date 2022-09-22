042b7a066d5d79657190affb1698ebaed96f41d5
Harry <harrypham_@hotmail.com>
2022-09-22 18:05:34 +1000

    fix index

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Contact from './src/Components/Contact/Contact'
 import { appContainer, test, container, gap } from './src/styles/app.css'
 import clsx from 'clsx';
 
-const App: NextPage = () => {
+const App: any = () => {
   return (
     <div className={clsx(appContainer, container)}>
       <Home></Home>
